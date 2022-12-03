61d6b61856df0a59e2521f2a5b2c39258f06ec2a
FifthLord <nborysenko777@gmail.com>
2022-12-03 15:13:23 +0200

    add Nav page

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react';
+import { Context } from '../index';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
+import Navbar from 'react-bootstrap/Navbar';
+// import { NavLink } from 'react-router-dom';
+import { SHOP_ROUTE } from '../utils/consts';
+import { observer } from "mobx-react-lite"
+
+const NavBar = observer(() => {
+   const { user } = useContext(Context)
+   return (
+      <Navbar bg="dark" variant="dark">
+         <Container>
+            <Navbar.Brand href={SHOP_ROUTE}>КупиСлона</Navbar.Brand>
+            {/* <NavLink style={{ color: 'white' }} to={SHOP_ROUTE}>КупиСлона</NavLink> */}
+            {user.isAuth ?
+               <Nav className="ml-auto" style={{ color: 'white' }}>
+                  <Button variant={"outline-light"}>Адмін панель</Button>
+                  <Button variant={"outline-light"}>Увійти</Button>
+               </Nav>
+               :
+               <Nav className="ml-auto" style={{ color: 'white' }}>
+                  <Button variant={"outline-light"} onClick={() => user.setIsAuth(true)}>Авторизація</Button>
+               </Nav>
+            }
+
+         </Container>
+      </Navbar>
+   );
+});
+
+export default NavBar;
