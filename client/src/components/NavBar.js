eacf2e597b314ea74c006e7ef2282df52a84af77
FifthLord <nborysenko777@gmail.com>
2022-12-12 17:31:56 +0200

    fix NavBar page

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -5,11 +5,13 @@ import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
 import Navbar from 'react-bootstrap/Navbar';
 // import { NavLink } from 'react-router-dom';
-import { SHOP_ROUTE } from '../utils/consts';
+import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from '../utils/consts';
 import { observer } from "mobx-react-lite"
+import { useNavigate } from "react-router-dom"
 
 const NavBar = observer(() => {
    const { user } = useContext(Context)
+   const navigate = useNavigate()
    return (
       <Navbar bg="dark" variant="dark">
          <Container>
@@ -17,8 +19,15 @@ const NavBar = observer(() => {
             {/* <NavLink style={{ color: 'white' }} to={SHOP_ROUTE}>КупиСлона</NavLink> */}
             {user.isAuth ?
                <Nav className="ml-auto" style={{ color: 'white' }}>
-                  <Button variant={"outline-light"}>Адмін панель</Button>
-                  <Button variant={"outline-light"} className="ml-2">Увійти</Button>
+                  <Button
+                     variant={"outline-light"}
+                     onClick={() => navigate.push(ADMIN_ROUTE)}
+                  >Адмін панель</Button>
+                  <Button
+                     variant={"outline-light"}
+                     onClick={() => navigate.push(LOGIN_ROUTE)}
+                     className="ml-2"
+                  >Вийти</Button>
                </Nav>
                :
                <Nav className="ml-auto" style={{ color: 'white' }}>
