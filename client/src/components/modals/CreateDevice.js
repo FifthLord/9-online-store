7a03704ee5b02a3f6c51de298c685450ff775815
FifthLord <nborysenko777@gmail.com>
2022-12-13 22:24:00 +0200

    create modal windows

---
+++ client/src/components/modals/CreateDevice.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Button, Form, Modal } from 'react-bootstrap';
+
+const CreateDevice = ({ show, onHide }) => {
+   return (
+      <Modal
+         show={show}
+         onHide={onHide}
+         size="lg"
+         centered
+      >
+         <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+               Додати новий тип
+            </Modal.Title>
+         </Modal.Header>
+         <Modal.Body>
+            <Form>
+               <Form.Control
+                  placeholder={"Введіть назву типу"}
+               />
+            </Form>
+         </Modal.Body>
+         <Modal.Footer>
+            <Button variant="outline-danger" onClick={onHide}>Закрити</Button>
+            <Button variant="outline-success" onClick={onHide}>Додати тип</Button>
+         </Modal.Footer>
+      </Modal>
+   );
+};
+
+export default CreateDevice;
--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Button, Container } from 'react-bootstrap';
+import CreateBrand from '../components/modals/CreateBrand';
+import CreateDevice from '../components/modals/CreateDevice';
+import CreateType from '../components/modals/CreateType';
 
 const Admin = () => {
    return (
       <Container className='d-flex flex-column'>
-         <Button>Додати тип</Button>
-         <Button>Додати бренд</Button>
-         <Button>Додати пристрій</Button>
+         <Button variant={'outline-dark'} className='mt-4 p-2'>Додати тип</Button>
+         <Button variant={'outline-dark'} className='mt-4 p-2'>Додати бренд</Button>
+         <Button variant={'outline-dark'} className='mt-4 p-2'>Додати пристрій</Button>
+         <CreateBrand />
+         <CreateDevice />
+         <CreateType show={true} />
       </Container>
    );
 };
