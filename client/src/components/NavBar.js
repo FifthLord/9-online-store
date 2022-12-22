d36f2ab1bc33d37b885d196ffe4299838dba2e47
FifthLord <nborysenko777@gmail.com>
2022-12-22 19:32:01 +0200

    fix NavBar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -9,6 +9,12 @@ import { useNavigate } from "react-router-dom"
 const NavBar = observer(() => {
    const { user } = useContext(Context)
    const navigate = useNavigate()
+
+   const logOut = () => {
+      user.setUser({})
+      user.setIsAuth(false)
+   }
+
    return (
       <Navbar bg="dark" variant="dark">
          <Container>
@@ -24,12 +30,12 @@ const NavBar = observer(() => {
                   <Button
                      className="m-1"
                      variant={"outline-light"}
-                     onClick={() => navigate(LOGIN_ROUTE)}
+                     onClick={() => logOut()}
                   >Вийти</Button>
                </Nav>
                :
                <Nav className="ml-auto" style={{ color: 'white' }}>
-                  <Button variant={"outline-light"} onClick={() => user.setIsAuth(true)}>Авторизація</Button>
+                  <Button variant={"outline-light"} onClick={() => navigate(LOGIN_ROUTE)}>Авторизація</Button>
                </Nav>
             }
          </Container>
