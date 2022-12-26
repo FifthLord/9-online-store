231fedee2d880eed5d1be774aae2cb591c9a7a12
FifthLord <nborysenko777@gmail.com>
2022-12-26 17:13:36 +0200

    add CreateBrand logic

--- client/src/components/modals/CreateBrand.js
+++ client/src/components/modals/CreateBrand.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form, Modal } from 'react-bootstrap';
+import { createBrand } from '../../http/deviceAPI.js'
 
 const CreateBrand = ({ show, onHide }) => {
+   const [value, setValue] = useState('')
+   const addBrand = () => {
+      createBrand({ name: value }).then(data => {
+         setValue('')
+         onHide()
+      })
+   }
    return (
       <Modal
          show={show}
@@ -16,13 +24,15 @@ const CreateBrand = ({ show, onHide }) => {
          <Modal.Body>
             <Form>
                <Form.Control
+                  value={value}
+                  onChange={e => setValue(e.target.value)}
                   placeholder={"Введіть назву бренду"}
                />
             </Form>
          </Modal.Body>
          <Modal.Footer>
             <Button variant="outline-danger" onClick={onHide}>Закрити</Button>
-            <Button variant="outline-success" onClick={onHide}>Додати бренд</Button>
+            <Button variant="outline-success" onClick={addBrand}>Додати бренд</Button>
          </Modal.Footer>
       </Modal>
    );
